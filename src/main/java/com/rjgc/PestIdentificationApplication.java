d17723ea66a1b3633173ca97484feab1e966f8d6
XUANXUQAQ <xuanxusb@outlook.com>
2021-04-08 12:35:37 +0800

    init

---
+++ src/main/java/com/rjgc/PestIdentificationApplication.java
@@ -0,0 +1,13 @@
+package com.rjgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PestIdentificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PestIdentificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rjgc/PestIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PestIdentificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

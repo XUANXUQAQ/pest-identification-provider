5af38270f1a2205063d049d43e95ff5904bd9e6f
XUANXUQAQ <xuanxusb@outlook.com>
2021-04-08 21:05:37 +0800

    添加swagger

--- src/main/java/com/rjgc/dao/Orders.java
+++ src/main/java/com/rjgc/dao/Orders.java
@@ -1,8 +1,6 @@
 package com.rjgc.dao;
 
 import com.alibaba.fastjson.annotation.JSONField;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

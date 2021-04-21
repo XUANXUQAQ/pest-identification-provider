b91a0542841ec5857101ab3a930c6ddad2c94079
xuanxu <xuanxusb@outlook.com>
2021-04-22 00:29:39 +0800

    放行options

--- src/main/java/com/rjgc/configs/SpringSecurityConfig.java
+++ src/main/java/com/rjgc/configs/SpringSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -59,6 +60,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .antMatchers("/login").permitAll()
                 .anyRequest().hasRole("admin");
         if (csrfProperties.getCsrfDisabled()) {

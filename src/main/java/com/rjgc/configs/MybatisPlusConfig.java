237bdb4781995a29265c50ac3bf8f04d00a6ad1b
xuanxu <xuanxusb@outlook.com>
2021-05-30 01:15:17 +0800

    关闭打印sql

--- src/main/java/com/rjgc/configs/MybatisPlusConfig.java
+++ src/main/java/com/rjgc/configs/MybatisPlusConfig.java
@@ -1,12 +1,9 @@
 package com.rjgc.configs;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.Properties;
 
 /**
  * @author zhaoyunjie
@@ -23,16 +20,16 @@ public class MybatisPlusConfig {
         return new PaginationInterceptor();
     }
 
-    /**
-     * 打印 sql
-     */
-    @Bean
-    public PerformanceInterceptor PerformanceInterceptor() {
-        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-        //格式化sql语句
-        Properties properties = new Properties();
-        properties.setProperty("format", "false");
-        performanceInterceptor.setProperties(properties);
-        return performanceInterceptor;
-    }
+//    /**
+//     * 打印 sql
+//     */
+//    @Bean
+//    public PerformanceInterceptor PerformanceInterceptor() {
+//        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+//        //格式化sql语句
+//        Properties properties = new Properties();
+//        properties.setProperty("format", "false");
+//        performanceInterceptor.setProperties(properties);
+//        return performanceInterceptor;
+//    }
 }

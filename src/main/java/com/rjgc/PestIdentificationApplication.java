6e610f10e6eb5df49f6121cf03bea41ab5121e02
xuanxu <xuanxusb@outlook.com>
2021-06-17 23:11:24 +0800

    修改为spring cloud分布式系统

--- src/main/java/com/rjgc/PestIdentificationApplication.java
+++ src/main/java/com/rjgc/PestIdentificationApplication.java
@@ -3,11 +3,13 @@ package com.rjgc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.rjgc.mapper")
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class PestIdentificationApplication {
 
     public static void main(String[] args) {
